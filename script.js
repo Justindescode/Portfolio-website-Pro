d503f53308e3202d9e6dafcecd1560b9c731ebda
Justin-Vision <97095919+Justindescode@users.noreply.github.com>
2024-06-10 14:24:50 -0500

    finished building the mediaqueries and break points

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-  const menu = document.querySelector(".nav-links");
+  const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
